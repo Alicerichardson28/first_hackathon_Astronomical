a7acb6c3a7d77c9630bb07842c10559c064ea820
github username <github email address>
2020-02-04 12:44:50 -0700

    Added seeds to the project

--- Astronmical_Backend/db/seeds.rb
+++ Astronmical_Backend/db/seeds.rb
@@ -1,7 +1,152 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Constellation.destroy_all 
+
+cassiopeia = Constellation.create(
+    name: "Cassiopeia – The Queen",
+    photo: "https://i.ibb.co/Tw5JJ8s/cassiopeia.jpg", 
+    location: "Northern hemisphere",
+    fun_fact: "Cassiopeia is one of the brightest and most recognizable constellations in the night sky.",
+    mythology:"Cassiopeia was a queen whose legendary vanity and arrogance ultimately led to her downfall.",
+    main_stars:"Segin, Ruchbah, Gamma Cassiopeiae, Schedar, Caph"
+    )
+
+    
+    andromeda = Constellation.create(
+    name: "Andromeda – The Chained Princess",
+    photo: "https://i.ibb.co/vxYhwxm/andromeda-constellation.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The Andromedids meteor shower which occurs every November is named after Andromeda as it appears to emanate from the constellation.",
+    mythology:"Princess Andromeda was famed for her beauty and would eventually marry the hero Perseus.",
+    main_stars:"Almach, Mirach, Alpheratz, M31"
+    )
+
+    
+    cancer = Constellation.create(
+    name: "Cancer – The Crab",
+    photo: "https://i.ibb.co/6wSbzSQ/cancer-3.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "Most of the stars that make up Cancer are quite faint, as a result it is one of the dimmest constellations.",
+    mythology:"The myth is based around the character of Hercules, a divine hero and son of Zeus.",
+    main_stars:"Altarf, Acubens, Asellus Australis, Asellus Borealis, Iota Cancri"
+    )
+
+    
+    capricorn = Constellation.create(
+    name: "Capricorn – The Sea Goat",
+    photo: "https://i.ibb.co/ykXjL01/capricornus-constellation.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The area of the night sky in which Capricornus is located is known as the 'Sea'.",
+    mythology:"Capricornus is based on extremely ancient mythology from 4,000 years ago, it has always been associated with a half goat half fish creature.",
+    main_stars:"Deneb Algedi, Nashira, Prima & Secunda Giedi, Dabih"
+    )
+
+    
+    draco = Constellation.create(
+    name: "Draco – The Dragon",
+    photo: "https://i.ibb.co/9HpCMrN/draco-new.jpg", 
+    location: "Northern hemisphere",
+    fun_fact: "A small galaxy known as the Draco Dwarf exists within the constellation, it's a satellite of our own galaxy, the Milky Way.",
+    mythology:"A multi-headed dragon named Ladon, his duty was to guard the Gardens of the Hesperides, a beautiful orchard cultivated by the wife of Zeus, Hera.",
+    main_stars:"Thuban, Edasich, Aldhibah, Nodus Secundus, Grumium"
+    )
+
+    
+    gemini = Constellation.create(
+    name: "Gemini – The Twins",
+    photo: "https://i.ibb.co/jrVjKhw/gemini-3.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The stars Pollux and Castor, which represent the head of the twins, were viewed as protectors of sailors by ancient seafarers.",
+    mythology:"The constellation is associated with the Greek characters Castor and Pollux. They were twin brothers born of the same mother but different fathers.",
+    main_stars:"Pollux, Castor, Wasat, Mekbuda, Mebsuta, Alzirr"
+    )
+
+    
+    hercules = Constellation.create(
+    name: "Hercules – The Hero",
+    photo: "https://i.ibb.co/GTnfwSZ/hercules-3.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "Several planets have been detected orbiting stars within the Hercules constellation including an enormous gas giant with 8 times the mass of Jupiter.",
+    mythology:"Hercules was an illegitimate child of Zeus, the King of Gods. His mother was Alcmene, wife of the Theban general Amphitryon. ",
+    main_stars:"Zeta, Sarin, Kornephoros, Rasalgethi"
+    )
+
+    
+    leo = Constellation.create(
+    name: "Leo – The Lion",
+    photo: "https://i.ibb.co/n04N6zr/leo-3.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The brightest star in the constellation is Regulus, which has surface temperatures more than twice that of the sun.",
+    mythology:"Like several constellations Leo is based on the adventures of Hercules, a Greek mythological hero and son of Zeus.",
+    main_stars:"Denebola, Zosma, Chort, Regulus, Algieba"
+    )
+
+    
+    libra = Constellation.create(
+    name: "Libra – The Scales",
+    photo: "https://i.ibb.co/8cFRq5t/libra-constellation.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "Libra has been recognized as a constellation for at least 4,000 years.",
+    mythology:"The origins of Libra being perceived as scales goes back even further than ancient Greece, to the ancient Babylonians some 2,000 years earlier.",
+    main_stars:"Zuben Elschemali, Zuben Elgenubi, Brachium"
+    )
+
+    
+    lyra = Constellation.create(
+    name: "Lyra – The Harp",
+    photo: "https://i.ibb.co/tb8sF8r/lyra-2.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "Lyra is one of the smallest constellations but contains one of the brightest stars.",
+    mythology:"Lyra is based around the character of Orpheus, a famed musician with such extraordinary talent that he was even able to charm nature itself with his music.",
+    main_stars:"Vega, Sheliak, Sulafet"
+    )
+
+    
+    orion = Constellation.create(
+    name: "Orion – The Hunter",
+    photo: "https://i.ibb.co/TcBZtkC/orion-3.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The brightest star in the constellation is Rigel, which is 40,000 times brighter than the sun and emits 100,000 times the energy.",
+    mythology:"Orion was a giant hunter who was placed in the stars by Zeus upon his death.",
+    main_stars:"Betelgeuse, Meissa, Bellatrix, Alnitak, Alnilam"
+    )
+
+    
+    pegasus = Constellation.create(
+    name: "Pegasus – The Winged Horse",
+    photo: "https://i.ibb.co/0npN7xJ/pegasus-2.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The nearest star to Earth which could possibly explode as a supernova is located in the Pegasus constellation.",
+    mythology:"Pegasus is a winged stallion horse, a divine creature that brought forth springs from the Earth wherever it walked.",
+    main_stars:"Alpheratz, Scheat, Matar, Algenib"
+    )
+
+
+    perseus = Constellation.create(
+    name: "Perseus – The Medusa Killer",
+    photo: "https://i.ibb.co/dfDSRKn/perseus-2.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The annual Perseids meteor shower emanates from the direction of the constellation.",
+    mythology:"Once Perseus had reached adulthood he was sent on his most famous adventure, to slay Medusa, a snake haired Gorgon whose gaze could turn a man to stone.",
+    main_stars:"Gamma, Mirfak, Algol, Gorgonea Tertia"
+    )
+
+
+    pisces = Constellation.create(
+    name: "Pieces – The Fish",
+    photo: "https://i.ibb.co/D4BgFnn/pisces-constellation.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "The constellation represents the Greek Gods Aphrodite and Eros disguised as fish.",
+    mythology:"Aphrodite the Goddess of Beauty and her son Eros, the God of Love turned themselves into fish while fleeing Olympus",
+    main_stars:"Eta Piscium, Alrisha, Omega Piscium, Gamma Piscium"
+    )
+
+
+    saggittarius = Constellation.create(
+    name: "Sagittarius – The Archer",
+    photo: "https://i.ibb.co/zGXv1xh/sagittarius-5.jpg", 
+    location: "Northern and Southern hemisphere",
+    fun_fact: "It is thought that the super massive black hole that exists in the center of our galaxy is located within the Sagittarius constellation.",
+    mythology:"Some identify Sagittarius as the centaur Chiron, the son of Philyra and Cronus, who was said to have changed himself into a horse to escape his jealous wife, Rhea.",
+    main_stars:"Arkab, Rukbat, Ascella, Kaus Australis"
+    )
+
+
